288cbb604c0969413f8243fb4e47ec722ea54650
Takanori K <takataka.k0210@gmail.com>
2024-08-20 13:08:28 +0000

    finished 10.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,22 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
+
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
+
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
   end
+
 
   def new
     @user = User.new
   end
+
 
   def create
     @user = User.new(user_params)
@@ -28,8 +31,10 @@ class UsersController < ApplicationController
     end
   end
 
+
   def edit
   end
+
 
   def update
     if @user.update(user_params)
@@ -40,51 +45,60 @@ class UsersController < ApplicationController
     end
   end
 
+
   def destroy
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
 
+
   def edit_basic_info
     @user = User.find(params[:id])
   
     respond_to do |format|
-      format.html { render partial: 'users/edit_basic_info', locals: { user: @user } }
+      format.html { render partial: 'users/edit_basic_info', locals: { user: @user } } # 修正
       format.turbo_stream
     end
   end
 
+
+
+
   def update_basic_info
-      if @user.update(basic_info_params)
-        flash[:success] = "#{@user.name}の基本情報を更新しました。"
-      else
-        flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
-      end
-      
-      respond_to do |format|
-        format.html { redirect_to users_url }
-        format.turbo_stream
-      end
-  end
+    if @user.update(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
   
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.turbo_stream
+    end
+  end
+
+
+
+
+
+
   private
+
 
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
-    end
-    
-    def basic_info_params
-      params.require(:user).permit(:department, :basic_time, :work_time)
     end
 
 
     # beforeフィルター
 
+
     # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
+
 
     # ログイン済みのユーザーか確認します。
     def logged_in_user
@@ -95,13 +109,19 @@ class UsersController < ApplicationController
       end
     end
 
+
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
     end
 
+
     # システム管理権限所有かどうか判定します。
     def admin_user
       redirect_to root_url unless current_user.admin?
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
 end
